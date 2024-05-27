2b62254cb4b3baac2250d9217987396b216319de
miya <leticiabholanda@gmail.com>
2024-05-26 21:18:37 -0300

    identificação das placas completa

--- placa.py
+++ placa.py
@@ -1,71 +1,25 @@
-# 9/26
-
-# ceara:    HTX - HZA ok
-#           NQL - NRE ok 
-#           NUM - NVF ok 
-#           OCB - OCU
-#           OHX - OIQ
-#           ORN - OSV
-#           OZA
-#           PMA - POZ
-#           RIA - RIN
-#           SAN - SBV
-
-# maranhao: HOL - HQE ok
-#           NHA - NHT ok 
-#           NMP - NNI ok 
-#           NWS - NXQ ok
-#           OIR - OJQ
-#           OXQ - OXZ
-#           PSA - PTZ
-#           ROA - ROZ
-
-# piaui:    LVF - LWQ ok
-#           NHU - NIX ok
-#           ODU - OEI
-#           OUA - OUE
-#           OVW - OVY
-#           PIA - PIZ
-#           QRN - QRZ
-#           RSG - RST
-
-# a b c d e f g h i j k l m n o p q r s t u v w x y z
-
 def identificarEstado(placa): 
-    # trasnformando a string da placa em uma lista de chars
-    letrasPlaca = list(placa)
+    # intervalos e estado correspondente na forma (incio, fim, estado)
+    intervaloEstado =[('HOL', 'HQE', 'Maranhão'), ('HTX', 'HZA', 'Ceará'), ('LVF', 'LWQ', 'Piauí'), ('NHA', 'NHT', 'Maranhão'), 
+         ('NHU', 'NIX', 'Piauí'), ('NMP', 'NNI', 'Maranhão'), ('NQL', 'NRE', 'Ceará'), ('NUM', 'NVF', 'Ceará'), 
+         ('NWS', 'NXQ', 'Maranhão'), ('OCB', 'OCU', 'Ceará'), ('ODU', 'OEI', 'Piauí'), ('OHX', 'OIQ', 'Ceará'), 
+         ('OIR', 'OJQ', 'Maranhão'), ('ORN', 'OSV', 'Ceará'), ('OUA', 'OUE', 'Piauí'), ('OVW', 'OVY', 'Piauí'), 
+         ('OXQ', 'OXZ', 'Maranhão'), ('OZA', 'OZA', 'Ceará'), ('PIA', 'PIZ', 'Piauí'), ('PMA', 'POZ', 'Ceará'), 
+         ('PSA', 'PTZ', 'Maranhão'), ('QRN', 'QRZ', 'Piauí'), ('RIA', 'RIN', 'Ceará'), ('ROA', 'ROZ', 'Maranhão'), 
+         ('RSG', 'RST', 'Piauí'), ('SAN', 'SBV', 'Ceará')]
+    
+    primeiras3Letras = placa[:3]
 
-    segundaLetra = letrasPlaca[1]
-    terceiraLetra = letrasPlaca[2]
-    estado = "Não identificado"
-    CE = "Ceará"
-    MA = "Maranhão"
-    PI = "Piauí"
-
-    match letrasPlaca[0]: 
-        case 'H': 
-            #                   HT X, Y, Z                                  HU, HV, HW, HX, HY                                    HZA
-            if (segundaLetra == 'T' and terceiraLetra >= 'X') or (segundaLetra > 'T' and segundaLetra < 'Z') or (segundaLetra == 'Z' and terceiraLetra == 'A'):
-                estado = CE
-            #                HO L, M, ..., Y, Z                              HP                            HQ A, B, C, D, E
-            elif (segundaLetra == 'O' and terceiraLetra >= 'L') or (segundaLetra == 'P') or (segundaLetra == 'Q' and terceiraLetra <= 'E'):
-                estado = MA
+    for possibilidade in intervaloEstado:
+        # comparação lexicográfica entre as 3 primeiras letras da placa e o intervalo, feita para cada um dos intervalos
+        # possíveis para os três estados definidos
+        if primeiras3Letras >= possibilidade[0] and primeiras3Letras <= possibilidade[1]:
+            print(f'O estado de origem da placa {placa} é {possibilidade[2]}')
+            return
         
-        case 'L': 
-            #               LV F, G, ..., Y, Z                                LW A, B, ..., P, Q    
-            if (segundaLetra == 'V' and terceiraLetra >= 'F') or (segundaLetra == 'W' and terceiraLetra <= 'Q'): 
-                estado = PI
+    print(f'O estado de origem da placa não foi identificado')
+        
+    
 
-        case 'N':
-            #            NQ L, M, ..., Y, Z                                     NR A, B, C, D, E                            NU M, N, ..., Y, Z                                   NV A, B, C, D, E, F
-            if (segundaLetra == 'Q' and terceiraLetra >= 'L') or (segundaLetra == 'R' and terceiraLetra <= 'E') or (segundaLetra == 'U' and terceiraLetra >= 'M') or (segundaLetra == 'V' and terceiraLetra <= 'F'):
-                estado = CE
-            #                           NH A, B, ..., S, T                                              NM P, Q, ..., Y, Z                              NN A, B, ..., H, I                                   NW S, T, ..., Y, Z                                 NX A, B, ..., P, Q
-            elif (segundaLetra == 'H' and terceiraLetra <= 'A' and terceiraLetra <= 'T') or (segundaLetra == 'M' and terceiraLetra >= 'P') or (segundaLetra == 'N' and terceiraLetra <= 'I') or (segundaLetra == 'W' and terceiraLetra >= 'S') or (segundaLetra == 'X' and terceiraLetra <= 'Q'):
-                estado = MA
-            #               NH U, V, W, X, Y, Z                                NI A, B, ..., W, X
-            elif (segundaLetra == 'H' and terceiraLetra >= 'U') or (segundaLetra == 'I' and terceiraLetra <= 'X'): 
-                estado = PI
-        case 'O': 
-            pass
+    
             
