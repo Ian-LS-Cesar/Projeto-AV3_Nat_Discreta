5e97c4d69808f2c60de1c7698c415f9955f0c305
Ian-LS-Cesar <129915834+Ian-LS-Cesar@users.noreply.github.com>
2024-05-27 13:19:03 -0300

    Comentários extras removidos, código finalizado.

--- estacionamento.py
+++ estacionamento.py
@@ -4,30 +4,21 @@ class Estacionamento:
         self.valor_extra = valor_extra
         self.linhas = linhas
         self.colunas = colunas
-        # uma matriz pode ser simulada por uma list simples de linhas x colunas espaços
+        # A matriz pode ser simulada por uma list simples de linhas x colunas espaços
         self.estacionamento = ["       "] * (linhas * colunas)
-        # as placas tem que ser armazenadas no estacionamento, pois nao necessariamente saberemos o local em que 
-        # o carro estava estacionado, apenas sua placa, pensa como um leitor de placa na entrada e saída, e é a 
-        # única informação que teremos, além do horario de chegada e saida, óbvio que para fins práticos nós vamos 
-        # escolher onde o carro estaciona, mas em uma aplicação real isso nao ocorreria
-        # self.placas = {}
-        # nome mais descritivo, já que armazena apenas a hora de chegada. tempo_estacionamento da a entender que 
-        # armazenaria o tempo que o carro ficou, o que não é possível já que o horário de saída só é fornecido na 
-        # saída, quando o horário de chegada é poppado (então não da nem pra armazenar o valor e também não faria
-        # sentido)
         self.horarios_chegada = {}
 
     def mostrarEstacionamento(self):
         print()
         
-        # itera por cada carro e sua posição
+        # Itera por cada carro e sua posição
         for posicao, carro in enumerate(self.estacionamento):
-            # se o resto da posição pela quantidade e colunas for 0, a linha acabou, entao pula pra próxima, mas isso
+            # Se o resto da posição pela quantidade e colunas for 0, a linha acabou, entao pula pra próxima, mas isso
             # ocorre apenas após a posição 0
             if posicao % self.colunas == 0 and posicao != 0:
                 print('\n-------------------------------------------------')
 
-            # a divisão é só estética, tambem deixa mais visível as vagas livres
+            # Divisão estética, para deixar mais visíveis as vagas livres
             print(carro, end=" | ")
             
             
@@ -35,7 +26,6 @@ class Estacionamento:
     def adicionarCarro(self, linha, coluna, placa):
         if self.estacionamento[coluna + linha * self.colunas] == '       ':
             self.estacionamento[coluna + linha * self.colunas] = placa
-            # self.placas[(linha, coluna)] = placa
             hora_chegada = input("Hora de chegada (HH:MM): ")
             hora_chegada = list(map(int, hora_chegada.split(":")))
             self.horarios_chegada[placa] = hora_chegada
@@ -45,7 +35,6 @@ class Estacionamento:
 
     def removerCarro(self, placa):
         if placa in self.estacionamento:
-            # placa = self.placas.pop((linha, coluna))
             posicao = self.estacionamento.index(placa)
             self.estacionamento[posicao] = '       '
             hora_saida = input("Hora de saída (HH:MM): ")
@@ -59,7 +48,7 @@ class Estacionamento:
             print(f"\nCarro com placa {placa} não se encontra no estacionamento")
 
     def funcaoTeto(self, horas, minutos):
-        # tempo_extra = (horas - 3)
+        
         tempo_extra = horas
         if (minutos != 0):
             tempo_extra += 1
@@ -69,16 +58,7 @@ class Estacionamento:
         # Conversão do tempo
         tempo_chegada_em_minutos = hora_chegada[0] * 60 + hora_chegada[1]
         tempo_saida_em_minutos = hora_saida[0] * 60 + hora_saida[1]
-
-        # o caso que temos é um shopping, não teria nenhum carro que passaria a noite no estacionamento. além
-        # disso, para dar 24h o carro teria que chegar e sair na mesma hora (ex: de 23h para 1h do dia seguinte
-        # se passam apenas 2h, muito menos que 24, mas o código adicionaria 24h independentemente). considerando
-        # até mesmo uma aplicação real não faria sentido e deixaria o resultado errado. to deixando a explicação
-        # pra não chegar e só remover sem deixar explícito o motivo 
-        # Caso a saída foi no dia seguinte
-        # if tempo_saida_em_minutos < tempo_chegada_em_minutos:
-        #     tempo_saida_em_minutos += 24 * 60
-
+        
         diferenca_tempo_em_minutos = tempo_saida_em_minutos - tempo_chegada_em_minutos
         horas = diferenca_tempo_em_minutos // 60
         minutos = diferenca_tempo_em_minutos % 60
@@ -87,9 +67,7 @@ class Estacionamento:
         if (diferenca_tempo_em_minutos > 15):
             if (horas <= 3):
                 total_estacionamento = self.valor_fixo
-            else:
-                # normalmente, nesse caso, se passa o valor já pronto pra função teto, porque se ela for quem 
-                # subtrai 3 das horas, ela não é apenas uma função teto, ela faz mais do que o nome descreve 
+            else:               
                 total_estacionamento = self.valor_fixo + (self.funcaoTeto(horas - 3, minutos) * self.valor_extra)
         else:
             total_estacionamento = 0
@@ -106,8 +84,6 @@ class Estacionamento:
                 placa = input("Digite a placa: ")
                 self.adicionarCarro(linha, coluna, placa)
             elif acao == 'remover':
-                # linha = int(input("Digite a linha: "))
-                # coluna = int(input("Digite a coluna: "))
                 placa = input("\nDigite a placa: ")
                 self.removerCarro(placa)
                 
--- placa.py
+++ placa.py
@@ -1,18 +1,18 @@
 def identificarEstado(placa): 
-    # intervalos e estado correspondente na forma (incio, fim, estado)
+    # Intervalos e estado correspondente na forma (incio, fim, estado)
     intervaloEstado =[('HOL', 'HQE', 'Maranhão'), ('HTX', 'HZA', 'Ceará'), ('LVF', 'LWQ', 'Piauí'), ('NHA', 'NHT', 'Maranhão'), 
-         ('NHU', 'NIX', 'Piauí'), ('NMP', 'NNI', 'Maranhão'), ('NQL', 'NRE', 'Ceará'), ('NUM', 'NVF', 'Ceará'), 
-         ('NWS', 'NXQ', 'Maranhão'), ('OCB', 'OCU', 'Ceará'), ('ODU', 'OEI', 'Piauí'), ('OHX', 'OIQ', 'Ceará'), 
-         ('OIR', 'OJQ', 'Maranhão'), ('ORN', 'OSV', 'Ceará'), ('OUA', 'OUE', 'Piauí'), ('OVW', 'OVY', 'Piauí'), 
-         ('OXQ', 'OXZ', 'Maranhão'), ('OZA', 'OZA', 'Ceará'), ('PIA', 'PIZ', 'Piauí'), ('PMA', 'POZ', 'Ceará'), 
-         ('PSA', 'PTZ', 'Maranhão'), ('QRN', 'QRZ', 'Piauí'), ('RIA', 'RIN', 'Ceará'), ('ROA', 'ROZ', 'Maranhão'), 
-         ('RSG', 'RST', 'Piauí'), ('SAN', 'SBV', 'Ceará')]
+        ('NHU', 'NIX', 'Piauí'), ('NMP', 'NNI', 'Maranhão'), ('NQL', 'NRE', 'Ceará'), ('NUM', 'NVF', 'Ceará'), 
+        ('NWS', 'NXQ', 'Maranhão'), ('OCB', 'OCU', 'Ceará'), ('ODU', 'OEI', 'Piauí'), ('OHX', 'OIQ', 'Ceará'), 
+        ('OIR', 'OJQ', 'Maranhão'), ('ORN', 'OSV', 'Ceará'), ('OUA', 'OUE', 'Piauí'), ('OVW', 'OVY', 'Piauí'), 
+        ('OXQ', 'OXZ', 'Maranhão'), ('OZA', 'OZA', 'Ceará'), ('PIA', 'PIZ', 'Piauí'), ('PMA', 'POZ', 'Ceará'), 
+        ('PSA', 'PTZ', 'Maranhão'), ('QRN', 'QRZ', 'Piauí'), ('RIA', 'RIN', 'Ceará'), ('ROA', 'ROZ', 'Maranhão'), 
+        ('RSG', 'RST', 'Piauí'), ('SAN', 'SBV', 'Ceará')]
     
     primeiras3Letras = placa[:3]
 
     for possibilidade in intervaloEstado:
-        # comparação lexicográfica entre as 3 primeiras letras da placa e o intervalo, feita para cada um dos intervalos
-        # possíveis para os três estados definidos
+        # Comparação lexicográfica entre as 3 primeiras letras da placa e o intervalo, 
+        # feita para cada um dos intervalos possíveis para os três estados definidos
         if primeiras3Letras >= possibilidade[0] and primeiras3Letras <= possibilidade[1]:
             print(f'O estado de origem da placa {placa} é {possibilidade[2]}')
             return
